0978f7b0097eb3e21971ebc5ba42dba7b8336df7
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-09-30 17:01:39 +0200

    Class 4.5 Icons

--- src/ImageFilter.js
+++ src/ImageFilter.js
@@ -1,10 +1,21 @@
-import alimentacion from './assets/alimentacion.svg';
+import React from 'react';
+import alimentacion from './assets/images/alimentacion.svg';
+import salud from './assets/images/salud.svg';
+import otros from './assets/images/otros.svg';
+import transporte from './assets/images/transporte.svg';
+import utilidades from './assets/images/utilidades.svg';
 import { Icono } from './Components/UI';
 
-export default () => {
+export default (type) => {
     const Images = {
-        Restaurante: <Icono src={alimentacion} alt="icono restaurante"/>,
+        Restaurante: <Icono src={alimentacion} alt="Restaurante"/>,
+        Salud: <Icono src={salud} alt="Salud"/>,
+        Transporte: <Icono src={transporte} alt="Transporte"/>,
+        Utilidades: <Icono src={utilidades} alt="Utilidades"/>,
+        default: <Icono src={otros} alt="Otros"/>
     };
 
-    return Images;
+    return Images[type] || Images.default;
+
+    return <></>;
 };
